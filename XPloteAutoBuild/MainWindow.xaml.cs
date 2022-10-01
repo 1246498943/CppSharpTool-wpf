aa7433ef76d5264277eae2e82aef79878f50a527
1246498943 <1246498943@qq.com>
2022-10-01 18:43:16 +0800

    2

--- XPloteAutoBuild/MainWindow.xaml.cs
+++ XPloteAutoBuild/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace XPloteAutoBuild
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow() 
         {
              InitializeComponent();
         }
